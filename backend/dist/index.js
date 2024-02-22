1e14b1aa7cc42162b3ab7393e364ec8d05032fe6
tmachine <tmachine@sys13.PEC.IN>
2024-02-22 11:45:28 +0530

    Socket connected

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,17 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const http_1 = require("http");
+const socket_io_1 = require("socket.io");
 const node_path_1 = require("node:path");
 const parentDir = (0, node_path_1.join)("/", (0, node_path_1.dirname)(__dirname), "..");
 const hostname = '127.0.0.1';
 const port = 3000;
+const app = (0, express_1.default)();
 const server = (0, http_1.createServer)((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    console.log(req.method, req.url, req.headers['sec-ch-ua-platform']);
-    console.log(res);
-    res.end('Hello, World!\n');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
 });
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+io.listen(5173);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { createServer, request} from 'http';
-
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { dirname, join } from 'node:path';
 
 const parentDir = join("/", dirname(__dirname), "..");
@@ -8,14 +8,22 @@ const parentDir = join("/", dirname(__dirname), "..");
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  //res.setHeader('Server', '');
-  console.log(res);
-  res.end('Hello, World!\n');
+const app = express();
+const server = createServer((req, res)=>{
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
 });
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+});
+io.listen(5173);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 }); 
