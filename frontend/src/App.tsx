e9175a9ed3ec6f66bbb544e6b510bbe1352df6a5
tmachine <tmachine@sys13.PEC.IN>
2024-03-05 14:38:30 +0530

    Emptied the input field after clicking on send button. Now should authenticate the user before chatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,17 @@ function App() {//Type Assertion
   const [Message, setMessage] = useState('');
   var socket: Socket;
   useEffect(() => {
+<<<<<<< HEAD
   socket = io("http://localhost:3000", {
     autoConnect: false,
     reconnection: false,
   });
+=======
+    const socket = io("http://localhost:3000", {
+      autoConnect: false,
+      reconnection: false,
+    });
+>>>>>>> 66239a7 (Emptied the input field after clicking on send button. Now should authenticate the user before chatting)
   
   return () => {
     socket.connect();
@@ -18,9 +25,14 @@ function App() {//Type Assertion
 function handleClick(e: MouseEvent<HTMLButtonElement, Event>) {
   e.preventDefault(); //prevent refresh of the page
  
+<<<<<<< HEAD
   socket.emit('chat message', Message);
   setMessage('');
   //empties the input field
+=======
+  console.log(Message);
+  setMessage(''); //empties the input field
+>>>>>>> 66239a7 (Emptied the input field after clicking on send button. Now should authenticate the user before chatting)
 }
   return (
     <>
@@ -29,6 +41,5 @@ function handleClick(e: MouseEvent<HTMLButtonElement, Event>) {
     </>
   )
 }
-
 
 export default App;
