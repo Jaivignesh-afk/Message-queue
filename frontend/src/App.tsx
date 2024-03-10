099145beca50933a35757c058bfb5b635eb5c590
Jaivignesh-afk <jjaivignesh2002@gmail.com>
2024-03-10 16:12:56 +0530

    Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,10 @@ function App() {//Type Assertion
   const [Message, setMessage] = useState('');
   var socket: Socket;
   useEffect(() => {
-<<<<<<< HEAD
   socket = io("http://localhost:3000", {
     autoConnect: false,
     reconnection: false,
   });
-=======
-    const socket = io("http://localhost:3000", {
-      autoConnect: false,
-      reconnection: false,
-    });
->>>>>>> 66239a7 (Emptied the input field after clicking on send button. Now should authenticate the user before chatting)
   
   return () => {
     socket.connect();
@@ -25,14 +18,9 @@ function App() {//Type Assertion
 function handleClick(e: MouseEvent<HTMLButtonElement, Event>) {
   e.preventDefault(); //prevent refresh of the page
  
-<<<<<<< HEAD
   socket.emit('chat message', Message);
   setMessage('');
   //empties the input field
-=======
-  console.log(Message);
-  setMessage(''); //empties the input field
->>>>>>> 66239a7 (Emptied the input field after clicking on send button. Now should authenticate the user before chatting)
 }
   return (
     <>
